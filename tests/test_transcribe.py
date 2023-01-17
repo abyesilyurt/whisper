a4d3467dacb406c59c236f223b38a81f7742fc71
Jong Wook Kim <jongwook@nyu.edu>
2023-01-17 13:43:36 -0800

    allow test_transcribe to run on CPU when CUDA is not available

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -2,6 +2,7 @@ import io
 import os
 
 import pytest
+import torch
 
 import whisper
 from whisper.utils import write_vtt
@@ -33,7 +34,9 @@ def test_transcribe_callback():
     model = whisper.load_model("tiny.en")
     audio_path = os.path.join(os.path.dirname(__file__), "jfk.flac")
 
-    result = model.transcribe(audio_path, language="en", temperature=0.0, segment_callback=segment_callback)
+    result = model.transcribe(
+        audio_path, language="en", temperature=0.0, segment_callback=segment_callback
+    )
     assert result["language"] == "en"
 
     transcription = result["text"].lower()
